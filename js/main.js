837c9ecc5d2d0ca3861c059a09b591227bcc6cb3
xVistyx <wiktor.hc@gmail.com>
2022-07-18 13:03:52 +0200

    js-dlugosc-stringa

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let nazwa = 'Uwielbiam JavaScripta';
+let opis = 'Jestem świetnym programistą';
+
+function dluzszyString(_par1, _par2) {
+
+if (_par1.length > _par2.length) {
+    return _par1;
+
+}
+else {
+   return _par2;
+}
+
+
+
+}
+console.log(dluzszyString(nazwa, opis));
